c781b06b620a3b212f4bcb874d0b4896c6b6b9e4
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2020-04-29 01:55:07 +0600

    Login and Logout System Added

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
   require_once 'core/init.php';
 
-  $user = DB::getInstance()->get('users', array('id', '=', 1))->results();
-
-  print_r($user[0]->name);
-
   if(Session::exists('home')) {
     echo Session::flash('home');
   }
- ?>
+
+  $user = new User();
+  if($user->isLoggedIn()) {
+  ?>
+  <p>Hello <a href="#"><?php echo escape($user->data()->name); ?></a>!</p>
+  <ul>
+    <li><a href="logout.php">Log out</a> </li>
+  </ul>
+  <?php
+} else {
+  echo '<p>You need to <a href="login.php">log in</a> or <a href="register.php">register</a></p>';
+}
+?>
